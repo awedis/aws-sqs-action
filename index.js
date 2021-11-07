72b99fdd884ac2c96aef725bc430756955a2b0d8
Avo-Keofteian <avokeofteian@gmail.com>
2021-11-07 11:31:44 +0200

    feat: send message via sqs using aws-sdk

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const aws = require('aws-sdk');
 
-try {
-  const sqsUrl = core.getInput('url');
-  const message = core.getInput('message');
+async function run() {
+  try {
+    const SQS = new aws.SQS();
+    const sqsUrl = core.getInput('url');
+    const message = core.getInput('message');
+    const params = {
+      QueueUrl: sqsUrl,
+      MessageBody: message,
+    };
 
-  console.log('sqsUrl =>', sqsUrl);
-  console.log('message =>', message);
-} catch (error) {
-  core.setFailed(error.message);
+    SQS.sendMessage(params, (err, resp) => {
+      if (err) {
+          throw err;
+      } else {
+          console.log(`resp ${JSON.stringify(resp, null, 2)}`);
+      }
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+module.exports = run;
+if (require.main === module) {
+  run();
 }
